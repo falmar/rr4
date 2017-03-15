eecf78c8f612401f40d7abfe4b8229f93ec8d2a5
David Lavieri <daviddlavier@gmail.com>
2017-03-14 21:25:34 -0400

    Nav links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom'
+import {BrowserRouter, Route, NavLink as Link} from 'react-router-dom'
+
+import './App.css'
+
+const isActiveFunc = (match, location) => {
+  console.log(match, location)
+
+  return match
+}
 
 const Links = () => (
   <nav>
-    <Link to='/'>Home</Link> &nbsp;
-    <Link to={{pathname: '/about'}}>About</Link> &nbsp;
-    <Link replace to='/contact'>About</Link>
+    <Link exact activeClassName='active' to='/'>Home</Link>
+    <Link activeStyle={{color: 'green'}} to='/about'>About</Link>
+    <Link
+      activeClassName='active'
+      isActive={isActiveFunc}
+      to='/contact'>
+      Contact
+    </Link>
   </nav>
 )
 
