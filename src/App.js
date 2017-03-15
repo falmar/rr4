25b66765125245d095ce9aff9eb6899b50c020be
David Lavieri <daviddlavier@gmail.com>
2017-03-14 21:18:21 -0400

    Basic Nav

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
 
-const Home = props => {
-  console.log(props)
-
-  return <h1>Home</h1>
-}
-const About = () => <h1>About</h1>
+const Links = () => (
+  <nav>
+    <Link to='/'>Home</Link> &nbsp;
+    <Link to={{pathname: '/about'}}>About</Link> &nbsp;
+    <Link replace to='/contact'>About</Link>
+  </nav>
+)
 
 const App = () => (
   <BrowserRouter>
     <div>
+      <Links />
+
       <Route
         exact
         path='/'
-        component={Home} />
+        render={() => <h1>Home</h1>} />
       <Route
-        strict
-        path='/about/'
-        component={About} />
+        path='/about'
+        render={() => <h1>About</h1>} />
       <Route
         path='/contact'
         render={() => <h1>Contact</h1>} />
-      <Route
-        path='/signin'
-        children={({match}) => match && <h1>Sign In</h1>} />
     </div>
   </BrowserRouter>
 )
